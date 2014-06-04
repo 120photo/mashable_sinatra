47f6a70b66d670f4ab99b7cb4f988ab4008311e5
Jose Juarez <120photo@gmail.com>
2014-06-04 00:33:19 -0700

    created drop down menu with categories

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ get '/' do
     resp = RestClient.get('http://mashable.com/stories.json')
     doc = JSON.parse(resp)
     @stories = doc['new']
-    cats = []
+    cat = []
+    doc['new'].each do |chan|
+    	cat.push(chan['channel'])
+    end
+    @categories = cat.uniq
     erb :home
 end
