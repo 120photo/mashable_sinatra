35a146165857c4d502f53dd7832be0babeec70cf
Jose Juarez <120photo@gmail.com>
2014-06-04 21:00:50 -0700

    cleaned code up a bit

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
 require 'rest_client'
 
+# creating variable with content of collection
+resp = RestClient.get('http://mashable.com/stories.json?new_per_page=36')
+# converting json into somthing Ruby can read
+doc = JSON.parse(resp)
+
+# setting up array for categories
+cat = []
+# parsing through array looking for story's channel
+# then pushing categories to cat array
+doc['new'].each do |chan|
+	cat.push(chan['channel'])
+end
+
+stories = doc['new']
+
 get '/' do
-    resp = RestClient.get('http://mashable.com/stories.json')
-    doc = JSON.parse(resp)
-    @stories = doc['new']
-    cat = []
-    doc['new'].each do |chan|
-    	cat.push(chan['channel'])
-    end
-    @categories = cat.uniq
+	@jumbo = 'Mash Mash2'
+    @stories = stories
+    @categories = cat.uniq.sort
     erb :home
 end
