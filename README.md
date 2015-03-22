2ae5e82fd7d212d38a9f4430e7411cd22857a068
Jose Juarez <120photo@gmail.com>
2015-03-22 09:48:19 -0700

    cleaned up the code a bit

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-Mashable Sinatra
-================
-
-Homework assignment for class.
+# Mashable Sinatra
 
 run `ruby app.rb` and navigate to 127.0.0.1:4567 in your browser. You will see the latest stories from Mashable.
--- app.rb
+++ app.rb
@@ -1,29 +1,3 @@
-=begin
-
-Sinatra + APIs
-==========================
-
-Today we will cover:
-
-* Working with APIs in Postman
-* Integrating APIs into Sinatra
-
-Lab + Homework: Mashable Bookmarks
-==================================
-
-Use http://mashable.com/stories.json to build a front-end to mashable.
-
-* As a user I can see a list of all new stories
-* As a user I can see a thumbnail of each story
-* As a user I can filter the stories by category (can be a static list of categories)
-
-Bonus
------
-* As a user I can see tweets relating to an article (use the [Twitter Gem](https://github.com/sferik/twitter))
-* As a user I can filter the stories by author (dynamically generated)
-
-=end
-
 require 'sinatra'
 require 'rest_client'
 require 'json'
@@ -34,32 +8,32 @@ resp = RestClient.get('http://mashable.com/stories.json?new_per_page=60')
 doc = JSON.parse(resp)
 
 # setting up array for categories
-cat = []
+categories = []
 # parsing through array looking for story's channel
 # then pushing categories to cat array
 doc['new'].each do |chan|
-	cat.push(chan['channel'])
+  categories.push(chan['channel'])
 end
 
 stories = doc['new']
 
 
 get '/' do
-	@jumbo = 'Mash Mash2'
-    @stories = stories
-    @categories = cat.uniq.sort
-    erb :home
+  @jumbo = 'Mash Mash2'
+  @stories = stories
+  @categories = categories.uniq.sort
+  erb :home
 end
 
 # sorter = []
 # doc['new'].each do |c, s|
-# 	sorter.push(c['channel']  s['title'])
+#   sorter.push(c['channel']  s['title'])
 # end
 
 
 get '/sort/:categories' do
-	sort_array = []
-	@stories = stories
-	@categories = cat.uniq().sort
-	return "here are the #{cat}"
+  sort_array = []
+  @stories = stories
+  @categories = categories.uniq().sort
+  return "here are the #{categories}"
 end
